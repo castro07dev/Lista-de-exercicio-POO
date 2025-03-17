ce2ef742b334de7ea2e2e98a17dd5a801675376e
Daniel Castro <dandancastro3690@gmail.com>
2025-03-17 20:17:06 -0300

    Adicionar arquivos de projeto.

---
+++ Lista de exercicio-POO/Controller/CirculoController.cs
@@ -0,0 +1,39 @@
+﻿using Lista_de_exercicio_POO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_de_exercicio_POO.Controller
+{
+
+    // classe controladora para circulos relacionado a circulo
+    internal class CirculoController
+    {
+        // metodo publio estatico para calcular o circulo 
+        public static (string Message, MessageBoxIcon icone )CalculaCirculo(string input)
+        {
+            if (Double.TryParse(input, out double valorConvertido))
+            {
+                //cria instancia da classe circulo class
+                CirculoClass circulo = new CirculoClass(valorConvertido);
+
+
+                string resultado = $"Área:{circulo.CalcularArea()}\nPerimetro:{circulo.CalcularPerimetro()}";
+
+
+
+            }
+            else
+            {
+                //
+
+            }
+            
+        }
+
+    }
+
+}
---
+++ Lista de exercicio-POO/Models/CirculoClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_exercicio_POO.Models
+{   
+    internal class CirculoClass
+    {
+        // atributo privado, que nao pode ser acessado diretamente fora da classe
+        private double Raio {  get; set; }
+    
+        // metodo construtor que recebe raio como parametro
+        public CirculoClass(double Raio)
+        {
+            SetRaio (Raio);
+        }
+
+        private void SetRaio(double raio)
+        {
+            if (Raio > 0)
+            {
+                this.Raio = raio;
+            }
+            else
+            {
+                // lança uma execao caso o raio seja menor que zero
+                throw new ArgumentException("O raio deve ser número positivo"); 
+
+            }
+        }
+
+        // metodo publico que calcula a area
+        public double CalcularArea()
+        { 
+            return Math.PI*(this.Raio * this.Raio);
+            
+        }
+
+        // metodo que calcula o perimetro
+        public double CalcularPerimetro()
+        {
+            double ValoreCalcularPerimetro = 2 * Math.PI * this.Raio;
+            return ValoreCalcularPerimetro;
+        
+        }
+
+
+    }
+}
