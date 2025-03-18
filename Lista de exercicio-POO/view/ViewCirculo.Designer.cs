e00d49208fa5b9fe12dda2c329bf920aa299506b
Daniel Castro <dandancastro3690@gmail.com>
2025-03-17 21:32:00 -0300

    feat Atualização2

--- Lista de exercicio-POO/view/ViewCirculo.Designer.cs
+++ Lista de exercicio-POO/view/ViewCirculo.Designer.cs
@@ -28,13 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlPrincipal = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pnlPrincipal
+            // 
+            this.pnlPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.pnlPrincipal.Name = "pnlPrincipal";
+            this.pnlPrincipal.Size = new System.Drawing.Size(495, 311);
+            this.pnlPrincipal.TabIndex = 0;
             // 
             // ViewCirculo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 450);
+            this.ClientSize = new System.Drawing.Size(494, 311);
+            this.Controls.Add(this.pnlPrincipal);
             this.Name = "ViewCirculo";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +51,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlPrincipal;
     }
 }
 
